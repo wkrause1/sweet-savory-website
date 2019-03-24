a40698e8957c3c2b6332873a7bdab63192dc9e82
Will Krause <wkrause1@radford.edu>
2019-03-24 16:45:35 -0400

    Changed db tables

--- donuts/models.py
+++ donuts/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-class Product(models.Model):
+class Donuts(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.FloatField(default=0.00)
+    image = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Sandwiches(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.FloatField(default=0.00)
--- donuts/views.py
+++ donuts/views.py
@@ -3,13 +3,13 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
-from .models import Product
+from .models import *
 
 def index(request):
     return render(request, 'donuts/index.html')
 
 def donuts(request):
-    donuts = Product.objects.all()
+    donuts = Donuts.objects.all()
     context = {
         'donuts' : donuts
     }
